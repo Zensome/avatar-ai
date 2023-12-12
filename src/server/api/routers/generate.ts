531a0504f7e1b993559ba399bee5375fa74ea1c4
Tsung Jui Hsieh <ray.hsieh@smasoft.com.tw>
2023-12-12 22:25:07 +0800

    credits decrement functionality

--- src/server/api/routers/generate.ts
+++ src/server/api/routers/generate.ts
@@ -1,3 +1,4 @@
+import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 
 import {
@@ -7,14 +8,36 @@ import {
 } from "~/server/api/trpc";
 
 export const generateRouter = createTRPCRouter({
-  generateIcon: publicProcedure
+  generateIcon: protectedProcedure
     .input(
       z.object({
         prompt: z.string(),
       })
     )
-    .mutation(({ ctx, input }) => {
+    .mutation(async ({ ctx, input }) => {
       console.log("we are here", input.prompt);
+      // TODO: verify the user has enough credit
+      const { count } = await ctx.prisma.user.updateMany({
+        where: {
+          id: ctx.session.user.id, //replace with real id
+          credits: {
+            gte: 1,
+          },
+        },
+        data: {
+          credits: {
+            decrement: 1,
+          },
+        },
+      });
+
+      if (count <= 0) {
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: "You do not have enough credits",
+        });
+      }
+
       return {
         message: "success",
       };
